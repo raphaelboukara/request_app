ce78c8156d48094d0b4129da3d16b116a07419c6
Raphael Boukara <raphael.boukara@gmail.com>
2014-05-16 10:37:15 +0300

    authorisation to create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  http_basic_authenticate_with name: "admin", password: "ozadmin", except: [:index, :show]
+  http_basic_authenticate_with name: "admin", password: "ozadmin", except: [:index, :show, :new, :create]
 
   def index
     @users = User.all
