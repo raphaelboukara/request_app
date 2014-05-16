25fe49d23033542c3294560b31ddf61191c93c9c
Raphael Boukara <raphael.boukara@gmail.com>
2014-05-16 11:04:31 +0300

    test

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module RequestApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += ["fontawesome-webfont.ttf",
-                         "fonts/fontawesome-webfont.eot",
+                         "bootstrap/fontawesome-webfont.eot",
                          "fontawesome-webfont.svg",
                          "fontawesome-webfont.woff",
                          "glyphicons-halflings-regular",
